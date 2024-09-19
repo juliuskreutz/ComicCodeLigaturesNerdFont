79e1d0412970debc674aeb50ad1c81d7c2cf31de
Julius Kreutz <julius@kreutz.dev>
2024-09-19 14:12:55 +1000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Comic Code Ligatures Nerd Font";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "comic-code-ligatures-nerd-font";
+        version = "1";
+
+        src = nixpkgs.lib.sourceFilesBySuffices ./. [".otf"];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/share/fonts/opentype/NerdFonts
+          mv *.otf $out/share/fonts/opentype/NerdFonts
+
+          runHook postInstall
+        '';
+      };
+      packages.comic-code-ligatures-nerd-font = self.packages.default;
+    });
+}
